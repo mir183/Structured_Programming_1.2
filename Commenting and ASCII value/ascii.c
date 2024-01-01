861cf4ca144adddf93dda0e8a06cd7237b5b77c9
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-01-01 12:01:38 +0600

    Add files via upload

---
+++ Commenting and ASCII value/ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // ASCII value example
+    char ch = 'A';
+    printf("The ASCII value of '%c' is %d.\n", ch, ch);
+
+    return 0;
+}
---
+++ Commenting and ASCII value/comment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Single-line comment
+    printf("This is a C program with comments.\n");
+
+    /*
+    Multi-line comment
+    It can span multiple lines.
+    */
+    return 0;
+}
